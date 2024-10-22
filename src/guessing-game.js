430aaa2dc309b3ffadb44d4065da453e0ad758ca
safonovlg10 <safonovlg10@gmail.com>
2024-10-22 20:18:50 +0300

    feat: add result

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.sum = Math.round((this.max + this.min) / 2);
+        return this.sum;
     }
 
     lower() {
-
+        this.max = this.sum;
     }
 
     greater() {
-
+        this.min = this.sum;
     }
 }
 
